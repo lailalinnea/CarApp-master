2a8a5dabb44fea50fac74d7a6e84e1be4342aafb
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-04-22 11:58:35 +0200

    connected(?) a database(?

---
+++ CarApp/Database.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SQLite;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarApp
+{
+    class Database
+    {
+        public SQLiteConnection dbConn;
+        private string databaseFilename = "./car.db";
+
+        public Database()
+        {
+            if ()
+            {
+
+            }
+        }
+    }
+}
