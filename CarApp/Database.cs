2416aab657eded082a766da49e0a840f5df04546
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-04-28 09:22:38 +0200

    conntinued with coding daatabase

--- CarApp/Database.cs
+++ CarApp/Database.cs
@@ -14,10 +14,37 @@ namespace CarApp
 
         public Database()
         {
-            if ()
+            if (!File.Exists(databaseFilename))
             {
-
+                File.Create(databaseFilename);
             }
+            dbConn = new SQLiteConnection("Data Source=" + databaseFilename);
+        }
+
+        public void OpenConnection()
+        {
+            if (dbConn.State != System.Data.ConnectionState.Open)
+            {
+                dbConn.Open();
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (dbConn.State != System.Data.ConnectionState.Closed)
+            {
+                dbConn.Close();
+            }
+        }
+
+        public int AddCarRow(Car car)
+        {
+            string qInsert = "INSERT INTO car ('regNr','make','model','year','forSale') VALUES (@regNr, @make, @model, @year, @forSale);";
+
+            SQLiteCommand dbCommand = new SQLiteCommand(qInsert, dbConn);
+            OpenConnection();
+
+
         }
     }
 }
