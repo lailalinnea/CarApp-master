50039722a46331696e21a4fae7a3f7e644c138a6
Felicia Stenvall <felicia.stenvall2244@gmail.com>
2021-05-06 11:55:35 +0200

    :)

--- CarApp/Database.cs
+++ CarApp/Database.cs
@@ -44,7 +44,54 @@ namespace CarApp
             SQLiteCommand dbCommand = new SQLiteCommand(qInsert, dbConn);
             OpenConnection();
 
+            dbCommand.Parameters.AddWithValue(@"regNr", car.GetRegNr()); 
+            dbCommand.Parameters.AddWithValue(@"maid", car.GetMake()); 
+            dbCommand.Parameters.AddWithValue(@"model", car.GetModel()); 
+            dbCommand.Parameters.AddWithValue(@"year", car.GetYear());
 
+            dbCommand.Parameters.AddWithValue(@"forSale", (car.GetForSale() ? 1 : 0));
+            int result = dbCommand.ExecuteNonQuery(); 
+            CloseConnection(); 
+            return result;
         }
+
+        public List<Car> GetRowsFromCars()
+        {
+            List<Car> listOfCars = new List<Car>(); 
+            string qSelect = "SELECT * FROM car;"; 
+            
+            SQLiteCommand dbCommand = new SQLiteCommand(qSelect, dbConn); 
+            OpenConnection(); 
+            SQLiteDataReader res = dbCommand.ExecuteReader(); 
+            if (res.HasRows)
+            {
+                while( res.Read() ) 
+                {
+                    Car car = new Car(Convert.ToString(res["regNr"]),
+                        Convert.ToString(res["maid"]),
+                        Convert.ToString(res["model"]),
+                        Convert.ToInt32(res["year"]),
+                        Convert.ToBoolean(res["forSale"]));
+                    listOfCars.Add(car);
+                }
+            }
+            CloseConnection();
+            return listOfCars;
+        }
+
+        internal int RemoveCarRowByRegNr(string regNr)
+        {
+            string qDelete = "DELETE FROM car WHERE regNr = @regNr;";
+
+            SQLiteCommand dbCommand = new SQLiteCommand(qDelete, dbConn);
+            OpenConnection();
+
+            dbCommand.Parameters.AddWithValue(@"regNr", regNr);
+
+            int result = dbCommand.ExecuteNonQuery();
+            CloseConnection();
+            return result;
+        }
+
     }
 }
